657de79b998cf19f14609f68be029d0f03a178cd
gkeglevich <griffinkeglevich@gmail.com>
2019-01-16 14:23:42 -0500

    Made it fancier

--- parse_replay.py
+++ parse_replay.py
@@ -27,19 +27,12 @@ turn1
 # Look at the replay, get the history, and then delete. Explore later.
 def parse_replay(replay='p1-15-01-2019-18-42-30-1547595750490-1556220261.replay'):
     # Keep a memory of all of the states
-    memory = []
     with open('replays/{}'.format(replay), 'r') as f:
         _ = f.readline()
         conf = f.readline()
         _ = f.readline()
-
-        # Read in the states until EOF
-        s = f.readline()
-        while (s != ''):
-            game_state = gamelib.GameState(json.loads(conf), s)
-            memory.append(game_state)
-
-            s = f.readline()
+        # Get all the rest of the states
+        memory = [gamelib.GameState(json.loads(conf), x) for x in f.readlines()]
 
     return memory
 
