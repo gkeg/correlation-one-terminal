6f4471f71203e4af35aa58d7ecc2a593f0f0e868
gkeglevich <griffinkeglevich@gmail.com>
2019-01-16 13:55:41 -0500

    Modified parse replay to read until EOF

--- parse_replay.py
+++ parse_replay.py
@@ -1,8 +1,9 @@
 import json
 import sys
+import pdb
 
+# Workaround to get gamelib in
 sys.path.insert(0, './pickle-algo/')
-
 import gamelib
 
 s = ""
@@ -18,19 +19,30 @@ turn1
 ...
 """
 
-replay = 'p1-13-01-2019-12-04-37-1547399077700--1034977755.replay'
+# Tilman Testing
+# replay = 'p1-13-01-2019-12-04-37-1547399077700--1034977755.replay'
 
-with open('replays/{}'.format(replay), 'r') as f:
-    _ = f.readline()
-    conf = f.readline()
-    _ = f.readline()
-    s = f.readline()
+# Probably want some functionality to keep only one replay in the replay folder
 
-print(s)
+# Look at the replay, get the history, and then delete. Explore later.
+def parse_replay(replay='p1-15-01-2019-18-42-30-1547595750490-1556220261.replay'):
+    # Keep a memory of all of the states
+    memory = []
+    with open('replays/{}'.format(replay), 'r') as f:
+        _ = f.readline()
+        conf = f.readline()
+        _ = f.readline()
 
-game_state = gamelib.GameState(json.loads(conf) , s)
+        # Read in the states until EOF
+        s = f.readline()
+        while (s != ''):
+            game_state = gamelib.GameState(json.loads(conf), s)
+            memory.append(game_state)
 
-print(game_state.turn_number)
-print(game_state.my_health)
+            s = f.readline()
 
-	
+    return memory
+
+# For testing
+if __name__ == '__main__':
+    parse_replay()
