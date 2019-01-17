4563ac23afbacf90085a02ff304ef487ab5f17f1
Tilman Lindig <tilman.lindig@rogers.com>
2019-01-16 20:12:10 -0500

    remove pkl, assume 1 replay

--- parse_replay.py
+++ parse_replay.py
@@ -1,9 +1,10 @@
 import json
 import sys
 import pdb
+import os
 
 # Workaround to get gamelib in
-sys.path.insert(0, './pickle-algo/')
+sys.path.insert(0, './python-algo/')
 import gamelib
 
 s = ""
@@ -18,22 +19,28 @@ turn1
 ...
 """
 
-# Tilman Testing
-# replay = 'p1-13-01-2019-12-04-37-1547399077700--1034977755.replay'
-
 # Probably want some functionality to keep only one replay in the replay folder
 
 # Look at the replay, get the history, and then delete. Explore later.
-def parse_replay(replay='p1-15-01-2019-18-42-30-1547595750490-1556220261.replay'):
+# If you want a specific replay, you need to specify the file name
+# Otherwise we assume there is only one
+def parse_replay(replay=None):
     # Keep a memory of all of the states
-    with open('replays/{}'.format(replay), 'r') as f:
-        _ = f.readline()
-        conf = f.readline()
-        _ = f.readline()
-        # Get all the rest of the states
-        memory = [gamelib.GameState(json.loads(conf), x) for x in f.readlines()]
+	files = os.listdir('./replays/')
 
-    return memory
+	if not len(files):
+		return None
+
+	filename = replay if replay else files[0]
+	with open('replays/{}'.format(filename), 'r') as f:
+		_ = f.readline()
+		conf = f.readline()
+		_ = f.readline()
+		# Get all the rest of the states
+		memory =  [gamelib.GameState(json.loads(conf), x) for x in f.readlines()]
+
+	return memory
+
 
 # For testing
 if __name__ == '__main__':
