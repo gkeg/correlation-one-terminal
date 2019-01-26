09b1f79bd5c0f9d4bd431c1e99dd65e4e32967fa
Tilman Lindig <tilman.lindig@rogers.com>
2019-01-26 13:02:05 -0500

    team 6 emp rush?

---
+++ Team6/algo_strategy.py
@@ -0,0 +1,289 @@
+import gamelib
+import random
+import copy
+import time
+
+from defences import Defences
+
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        random.seed()
+
+    def on_game_start(self, config):
+        """
+        Read in config and perform any initial setup here
+        """
+        gamelib.debug_write('Configuring your custom algo strategy...')
+        self.config = config
+        global FILTER, ENCRYPTOR, DESTRUCTOR, PING, EMP, SCRAMBLER
+        self.filter, self.encryptor, self.destructor, self.ping, self.emp, self.scrambler = [i for i in range(6)]
+        FILTER = config["unitInformation"][0]["shorthand"]
+        ENCRYPTOR = config["unitInformation"][1]["shorthand"]
+        DESTRUCTOR = config["unitInformation"][2]["shorthand"]
+        PING = config["unitInformation"][3]["shorthand"]
+        EMP = config["unitInformation"][4]["shorthand"]
+        SCRAMBLER = config["unitInformation"][5]["shorthand"]
+
+        self.unique_locs = []
+        self.emp_num = 0
+        self.defences = Defences(config)
+        self.prev_state = None
+
+    def on_turn(self, turn_state):
+        """
+        This function is called every turn with the game state wrapper as
+        an argument. The wrapper stores the state of the arena and has methods
+        for querying its state, allocating your current resources as planned
+        unit deployments, and transmitting your intended deployments to the
+        game engine.
+        """
+        game_state = gamelib.AdvancedGameState(self.config, turn_state)
+
+        gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
+        game_state.suppress_warnings(True)  # Uncomment this line to suppress warnings.
+
+        emp_cheese = True if self.emp_num > 1 and len(self.unique_locs) == 1 else False
+        if emp_cheese:
+            d, f = self.can_block(self.unique_locs[0], copy.deepcopy(turn_state))
+        self.defences.build_template(game_state, self.prev_state, emp_cheese)
+        self.prev_state = copy.deepcopy(game_state)
+
+        if game_state.turn_number != 0:
+            try:
+                self.best_spawn(game_state)
+            except Exception:
+                self.attack(game_state)
+
+        game_state.submit_turn()
+
+    def can_block(self, loc, state: gamelib.AdvancedGameState):
+
+        target = state.game_map.BOTTOM_LEFT if loc[1] > 13 else state.game_map.BOTTOM_RIGHT
+        path = state.find_path_to_edge(loc, target)
+
+        for i in path:
+            if i[1] < 17:
+                f = -1 if target == state.game_map.BOTTOM_RIGHT else 1
+                return [i[0], 13], [i[0] + f, 13]
+
+        return None, None
+
+    def on_initial_action(self, game_state):
+        if 'events' in game_state:
+            if 'spawn' not in game_state['events'] or len(game_state['events']['spawn']) == 0:
+                return
+            # if 'spawn' in curr['events']:
+            self.emp_num = 0
+            self.unique_locs = []
+            for unit in game_state['events']['spawn']:
+                loc, typ, _, player = unit
+
+                if typ == EMP and player == 2 and loc[1] > 13:
+                    # debug_write(loc, player)
+                    self.emp_num += 1
+                    if loc not in self.unique_locs:
+                        self.unique_locs.add(loc)
+
+    def best_spawn(self, state):
+        self.time_start = time.time()
+        locs = [[24, 10], [3, 10], [13, 0], [11, 2], [22, 8]]
+        units = [EMP, PING, SCRAMBLER]
+        bits = state.get_resource(state.BITS)
+
+        # og_map = copy.deepcopy(state.game_map)
+
+        best = (0, None)
+        for loc in locs:
+            for unit in units:
+                cost = 3 if unit == EMP else 1
+                if not state.can_spawn(unit, loc):
+                    continue
+
+                if time.time() - self.time_start < 2:
+                    val = self.simulate(copy.deepcopy(state), unit, loc, int(bits / cost))
+                    if val > best[0]:
+                        best = (val, (loc, unit, int(bits / cost)))
+                    else:
+                        break
+
+        top = 9 if state.turn_number <= 6 else 15
+
+        if best[0] > 0:
+            loc, unit, num = best[1]
+            if best[0] > num * cost or state.get_resource(state.BITS) >= top:
+                state.attempt_spawn(unit, loc, num)
+        else:
+            self.attack(state)
+
+    def simulate(self, state: gamelib.AdvancedGameState, unit_type, spawn_loc=(13, 0), num_units=1):
+        map = state.game_map
+
+        """
+        TODO: ENCRYPTORS
+        """
+        target_edge = map.TOP_RIGHT if spawn_loc[0] < 14 else map.TOP_LEFT
+
+        path = state.find_path_to_edge(spawn_loc, target_edge)
+
+        total_dmg = 0
+        total_cores = 0
+
+        frames = 2 if unit_type == PING else 4
+        dmg = 0 if unit_type == SCRAMBLER else (1 if unit_type == PING else 3)
+
+        for i in range(num_units):
+            map.add_unit(unit_type, spawn_loc)
+        idx = 0
+
+        remaining = map[spawn_loc]
+
+        map.remove_unit(spawn_loc)
+        pings = 0
+        while idx < len(path):
+
+            loc = path[idx]
+
+            for i in remaining:
+                i.x = loc[0]
+                i.y = loc[1]
+                map[loc].append(i)
+
+            for i in range(frames):
+                defense_dmg = 4 * len(state.get_attackers(loc, 0))
+                our_dmg = len(map[loc]) * dmg
+
+                initial = len(map[loc])
+
+                if map[loc]:
+                    target = state.get_target(map[loc][0])
+                else:
+                    break
+
+                if target:
+                    target.stability -= our_dmg
+                    total_dmg += our_dmg
+                    total_cores += (our_dmg / target.max_stability) * target.cost
+                    if target.stability < 0:
+                        map.remove_unit([target.x, target.y])
+
+                dead = 0
+                for i in range(initial):
+
+                    if defense_dmg <= 0 or dead == len(map[loc]):
+                        break
+
+                    # Killed one, did they kill more
+                    if defense_dmg >= map[loc][dead].stability:
+                        defense_dmg -= map[loc][dead].stability
+                        dead += 1
+
+                    # They didn't kill this one
+                    else:
+                        map[loc][dead].stability -= defense_dmg
+                        break
+
+                # kill the dead ones
+                for i in range(dead):
+                    map[loc].pop(0)
+
+            remaining = map[loc]
+            pings = remaining
+
+            if len(remaining) == 0:
+                break
+
+            map.remove_unit(loc)
+
+            idx += 1
+
+            if loc[1] > 14:
+                mod_path = state.find_path_to_edge(spawn_loc, target_edge)
+                if path != mod_path and loc in mod_path:
+                    idx = mod_path.index(loc) + 1
+                    path = mod_path
+
+        return 3.5 * len(pings) + total_cores
+
+    # Attack with EMPs lol
+    def attack(self, state: gamelib.AdvancedGameState):
+        emp_loc = [3, 10]
+        while state.can_spawn(EMP, emp_loc, 1):
+            state.attempt_spawn(EMP, emp_loc, 1)
+
+    # Build our static defence template
+    def build_defences(self, state: gamelib.AdvancedGameState):
+        # When we're at full build, do nothing!
+        while state.get_resource(state.CORES) > 0 and not self.defences.build_done():
+            loc, defence_type = self.defences.get_next_defence()
+            if state.can_spawn(defence_type, loc):
+                state.attempt_spawn(defence_type, loc)
+
+    # Check if we need to do an anti-blackbeard defence
+    def anti_blackbeard(self, state: gamelib.AdvancedGameState):
+        return True
+
+    def blackbeard(self, state: gamelib.AdvancedGameState, left=False):
+        # Do initial first turn setup
+        # Spawn front destructors and encryptors
+        destr = [[23, 13], [24, 13]]
+        encr = [[23, 12], [24, 12], [25, 13], [23, 11]]
+        for i in destr:
+            if state.can_spawn(DESTRUCTOR, i):
+                state.attempt_spawn(DESTRUCTOR, i)
+
+        for i in encr:
+            if state.can_spawn(ENCRYPTOR, i):
+                state.attempt_spawn(ENCRYPTOR, i)
+
+        end = 12 if state.turn_number > 0 else 19
+
+        # open = False
+        # spawn remaining filters
+        for i in range(22, end, -1):
+            # if not state.contains_stationary_unit([i,i-12]) and state.get_resource(state.CORES) < 1:
+            #     open = True
+            if state.can_spawn(FILTER, [i, i - 12]):
+                state.attempt_spawn(FILTER, [i, i - 12])
+
+        # if we have extra cores use some of them18,13,9,5 then filters between
+        prio = [[1, 13], [1, 12], [0, 13], [18, 10], [18, 11], [13, 11], [9, 10], [9, 11], [5, 11]]
+        which = [DESTRUCTOR, DESTRUCTOR, FILTER, DESTRUCTOR, FILTER, DESTRUCTOR, DESTRUCTOR, FILTER, DESTRUCTOR]
+
+        for i in range(len(prio)):
+            if state.get_resource(state.CORES) > 4:
+                if state.can_spawn(which[i], prio[i]):
+                    state.attempt_spawn(which[i], prio[i])
+
+        i = 0
+        # Try to spawn some random stuff
+        while i < 10 and state.get_resource(state.CORES) > 6:
+            row = random.randint(10, 11)
+            col = random.randint(13 - row, 19)
+            type = DESTRUCTOR if random.randint(0, 1) else FILTER
+            if state.can_spawn(type, [row, col]):
+                state.attempt_spawn(type, [row, col])
+
+        if state.turn_number == 0:
+            state.attempt_spawn(PING, [13, 0], 2)
+            state.attempt_spawn(PING, [14, 0], 2)
+        # elif open:
+        #     state.attempt_spawn(EMP, [17, 3])
+        elif state.turn_number % 2:
+            bits = state.get_resource(state.BITS)
+            if bits > 4:
+                state.attempt_spawn(PING, [14, 0], 4)
+            state.attempt_spawn(PING, [13, 0], int(state.get_resource(state.BITS)))
+
+    def filter_blocked_locations(self, locations, game_state):
+        filtered = []
+        for location in locations:
+            if not game_state.contains_stationary_unit(location):
+                filtered.append(location)
+        return filtered
+
+
+if __name__ == "__main__":
+    algo = AlgoStrategy()
+    algo.start()
---
+++ Team6/defences.py
@@ -0,0 +1,285 @@
+# Defences class, will hold the priorities for everything, and find out
+# what kind of shit to build next. Put reactive defence stuff in here
+import gamelib
+from gamelib import debug_write
+
+import copy
+
+
+class Defences:
+    def __init__(self, config):
+        """ Constructor.
+
+        :param config: config object.
+        """
+        global FILTER, ENCRYPTOR, DESTRUCTOR, PING, EMP, SCRAMBLER, TEMPLATE_MASK
+        FILTER = config["unitInformation"][0]["shorthand"]
+        ENCRYPTOR = config["unitInformation"][1]["shorthand"]
+        DESTRUCTOR = config["unitInformation"][2]["shorthand"]
+        PING = config["unitInformation"][3]["shorthand"]
+        EMP = config["unitInformation"][4]["shorthand"]
+        SCRAMBLER = config["unitInformation"][5]["shorthand"]
+
+        self.config = config
+        self.UNIT_HEALTH = self.get_unit_params('stability')
+        self.UNIT_COSTS = self.get_unit_params('cost')
+        self.REMOVE_HEALTH_THRESH = 0.3
+        self.BUILD_DESTRUCTOR_WALLS_THRESH = 30
+        self.CORNER_DAMAGE_THRESH = 30
+
+        # What reactive defence can do, is look at the map, and see if it's a filter.
+        # If it's a filter and was attacked by pings and died, replace with a DEST.
+        # Otherwise, just do regular build
+
+        # Location mapped to a tuple of <UNIT, Priority>
+        self.BASE_TEMPLATE = [
+            # Chunk 1 --> Initial defences
+            ([2, 11],  DESTRUCTOR),
+            ([6, 11],  DESTRUCTOR),
+            ([11, 11], DESTRUCTOR),
+            ([16, 11], DESTRUCTOR),
+            ([21, 11], DESTRUCTOR),
+            ([25, 11], DESTRUCTOR),
+            ([0, 13],  FILTER),
+            ([1, 12],  FILTER),
+            ([26, 12], FILTER),
+            ([27, 13], FILTER),
+
+            # Chunk 2 --> Building out the first wall
+            ([3, 11],  FILTER),
+            ([4, 11],  FILTER),
+            ([5, 11],  FILTER),
+            ([7, 11],  FILTER),
+            ([8, 11],  FILTER),
+            ([9, 11],  FILTER),
+            ([10, 11], FILTER),
+            ([12, 11], FILTER),
+            ([13, 11], FILTER),
+            ([23, 11], DESTRUCTOR),
+            ([14, 11], FILTER),
+            ([15, 11], FILTER),
+            ([17, 11], FILTER),
+            ([4, 9],   ENCRYPTOR),
+            ([18, 11], FILTER),
+            ([19, 11], FILTER),
+            ([20, 11], FILTER),
+            ([22, 11], FILTER),
+            ([23, 11], FILTER),
+
+            # Chunk 3 --> Encryptors + Some more defences
+            ([25, 12], DESTRUCTOR),
+            ([5, 9],   ENCRYPTOR),
+            ([24, 13], FILTER),
+            ([23, 13], FILTER),
+            ([22, 13], FILTER),
+            ([21, 13], FILTER),
+            ([20, 13], FILTER),
+            ([19, 13], FILTER),
+            ([18, 13], FILTER),
+            ([17, 13], FILTER),
+            ([22, 9],  ENCRYPTOR),
+            ([23, 9],  ENCRYPTOR),
+            ([16, 13], FILTER),
+            ([15, 13], FILTER),
+            ([14, 13], FILTER),
+            ([13, 13], FILTER),
+            ([12, 13], FILTER),
+            ([11, 13], FILTER),
+
+        ]
+        self.TEMPLATE_MASK = [item[0] for item in self.BASE_TEMPLATE]
+        self.LEFT_CORNER_MASK = [
+            [0, 13], [1, 13], [2, 13],
+                     [1, 12], [2, 12],
+                              [2, 11],
+        ]
+        self.RIGHT_CORNER_MASK = [
+            [25, 13], [26, 13], [27, 13],
+            [25, 12], [26, 12],
+            [25, 11]
+        ]
+        self.R_CORNER_ATTACKED, self.L_CORNER_ATTACKED = False, False
+
+    def get_unit_params(self, param):
+        """ Extracts config parameters of units into a python dictionary.
+
+        :param param: parameter key.
+        :return: (dict)
+        """
+        params = {}
+
+        for unit in self.config["unitInformation"]:
+            if unit['display'] == 'Remove':
+                continue
+            else:
+                params[unit['shorthand']] = unit[param]
+        return params
+
+    def find_low_health_units(self, state: gamelib.AdvancedGameState, search_mask, health_thresh):
+        """ Find all static units that lost some health during last round.
+
+        :param state: current game state.
+        :param search_mask: list of locations - mask where to  search for damaged units.
+        :param health_thresh: threshold for filtering low health units.
+        :return: (list) locations of damaged units.
+        """
+        damaged_locs = []
+
+        for loc in search_mask:
+            # Find defensive unit at current location.
+            curr_unit = [unit for unit in state.game_map[loc]
+                         if unit.unit_type in [DESTRUCTOR, FILTER, ENCRYPTOR]]
+            curr_unit = None if len(curr_unit) == 0 else curr_unit[0]
+            if curr_unit is not None:
+                health_limit = health_thresh * self.UNIT_HEALTH[curr_unit.unit_type]
+
+                if curr_unit.stability < health_limit:
+                    damaged_locs.append(loc)
+
+        return damaged_locs
+
+    @staticmethod
+    def destroy_low_health_units(state, locs):
+        """ Removes units provided in locs list.
+
+        :param state: state object.
+        :param locs: list of locations to be removed.
+        :return:
+        """
+        for loc in locs:
+            state.attempt_remove(loc)
+
+    @staticmethod
+    def check_corner(curr_state, prev_state, corner_mask):
+        damage_sum = 0
+
+        for loc in corner_mask:
+            prev_units = prev_state.game_map[loc]
+            curr_units = curr_state.game_map[loc]
+            prev_unit = None
+            prev_type = None
+            curr_unit = None
+
+            if prev_units is None or prev_units == []:
+                # If unit did not exist here in last round, ignore location.
+                continue
+            else:
+                for unit in prev_units:
+                    if unit.unit_type in [DESTRUCTOR, FILTER, ENCRYPTOR]:
+                        # If we find a defensive unit, then remember it.
+                        prev_unit = unit
+                        prev_type = unit.unit_type
+                        break
+
+            if prev_unit:
+                if curr_units is None or curr_units == []:
+                    # If we had a unit in prev round, but not in this one, increment damage.
+                    damage_sum += prev_unit.stability
+                else:
+                    for unit in curr_units:
+                        if unit.unit_type == prev_type:
+                            # If we find a defensive unit of same type as last round, remember it.
+                            curr_unit = unit
+                            break
+                    # Increment damage taken based on change in unit stability.
+                    damage_sum += curr_unit.stability - prev_unit.stability
+
+        return damage_sum
+
+    def were_corners_attacked(self, curr_state, prev_state):
+        """ Checks if corners were attacked in the last round.
+
+        :param curr_state: state object.
+        :param prev_state: state object.
+        """
+        left_damage, right_damage = 0, 0
+
+        if not self.R_CORNER_ATTACKED:
+            right_damage = max(0, self.check_corner(curr_state, prev_state, self.RIGHT_CORNER_MASK))
+            debug_write("Right damage ", right_damage)
+
+        if not self.L_CORNER_ATTACKED:
+            left_damage = max(0, self.check_corner(curr_state, prev_state, self.LEFT_CORNER_MASK))
+            debug_write("Left damage ", left_damage)
+
+        self.R_CORNER_ATTACKED = right_damage > self.CORNER_DAMAGE_THRESH
+        self.L_CORNER_ATTACKED = left_damage > self.CORNER_DAMAGE_THRESH
+
+    def build_template(self, state: gamelib.AdvancedGameState, prev_state, emp_threat):
+        """ Adds any defences to the priority queue if we were destroyed.
+
+        :param state: game state.
+        :param prev_state: game state.
+        """
+        if prev_state:
+            self.were_corners_attacked(state, prev_state)
+            debug_write(
+                "Right Corner State: {}".format(self.R_CORNER_ATTACKED),
+                "Left Corner State: {}".format(self.L_CORNER_ATTACKED)
+            )
+            #
+            # if self.R_CORNER_ATTACKED:
+            #     for i in range(len(self.BASE_TEMPLATE)):
+            #         if self.BASE_TEMPLATE[i][0] in self.RIGHT_CORNER_MASK:
+            #             debug_write("Updating Template ", self.BASE_TEMPLATE[i])
+            #             self.BASE_TEMPLATE[i] = (self.BASE_TEMPLATE[i][0], DESTRUCTOR)
+            #
+            # if self.L_CORNER_ATTACKED:
+            #     for i in range(len(self.BASE_TEMPLATE)):
+            #         if self.BASE_TEMPLATE[i][0] in self.LEFT_CORNER_MASK:
+            #             debug_write("Updating Template ", self.BASE_TEMPLATE[i])
+            #             self.BASE_TEMPLATE[i] = (self.BASE_TEMPLATE[i][0], DESTRUCTOR)
+            if self.R_CORNER_ATTACKED:
+                for i in range(len(self.BASE_TEMPLATE)):
+                    if self.BASE_TEMPLATE[i][0] in self.RIGHT_CORNER_MASK:
+                        self.BASE_TEMPLATE[i] = (self.BASE_TEMPLATE[i][0], DESTRUCTOR)
+
+                new_units = []
+                for loc in self.RIGHT_CORNER_MASK:
+                    if loc not in self.TEMPLATE_MASK:
+                        new_units.append((loc, DESTRUCTOR))
+                self.BASE_TEMPLATE = new_units + self.BASE_TEMPLATE
+
+            if self.L_CORNER_ATTACKED:
+                for i in range(len(self.BASE_TEMPLATE)):
+                    if self.BASE_TEMPLATE[i][0] in self.LEFT_CORNER_MASK:
+                        self.BASE_TEMPLATE[i] = (self.BASE_TEMPLATE[i][0], DESTRUCTOR)
+
+                new_units = []
+                for loc in self.LEFT_CORNER_MASK:
+                    if loc not in self.TEMPLATE_MASK:
+                        new_units.append((loc, DESTRUCTOR))
+                self.BASE_TEMPLATE = new_units + self.BASE_TEMPLATE
+
+        for unit in self.BASE_TEMPLATE:
+            if state.get_resource(state.CORES) > self.UNIT_COSTS[unit[1]]:
+                use_destruct = state.get_resource(state.CORES) > self.BUILD_DESTRUCTOR_WALLS_THRESH
+                unit_type = DESTRUCTOR if use_destruct else unit[1]
+                if state.can_spawn(unit_type, unit[0]):
+                    state.attempt_spawn(unit_type, unit[0])
+
+        low_health_units = self.find_low_health_units(
+            state, self.TEMPLATE_MASK, self.REMOVE_HEALTH_THRESH)
+        self.destroy_low_health_units(state, low_health_units)
+
+    # def find_prev_unit_states(self, search_mask):
+    #     """ Find states of units under search mast in the previous state.
+    #
+    #     :param search_mask: list of locations where to search for states.
+    #     :return: list of (location, unit) pairs.
+    #     """
+    #     prev_units = []
+    #     prev_locs = []
+    #
+    #     for loc in search_mask:
+    #         units = self.prev_state.game_map[loc]
+    #
+    #         if units is None or units == []:
+    #             continue
+    #         else:
+    #             for unit in units:
+    #                 prev_units.append(unit)
+    #                 prev_locs.append(loc)
+    #
+    #     return zip(prev_locs, prev_units)
+
